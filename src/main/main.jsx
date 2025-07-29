5933b195877c23e3baaaafdc770ae6be21f43619
frontdevuz <davlatovizzatillo7@gmail.com>
2025-07-29 15:16:44 +0500

    working with react router dom

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import About from "../pages/about/About";
-import Skills from "../pages/skills/Skills";
-import Projects from "../pages/projects/Projects";
-import Achievements from "../pages/achievements/Achievements";
-import Resume from "../pages/resume/Resume";
-import Contact from "../pages/contact/Contact";
+import AboutID from "../pages/about/About";
+import SkillsID from "../pages/skills/Skills";
+import ProjectsID from "../pages/projects/Projects";
+import AchievementsID from "../pages/achievements/Achievements";
+import ResumeID from "../pages/resume/Resume";
+import ContactID from "../pages/contact/Contact";
 import { Routes, Route } from "react-router-dom";
 import NavbarID from "../components/navbarFL/Navbar";
-
 export default function Main() {
   return (
     <>
